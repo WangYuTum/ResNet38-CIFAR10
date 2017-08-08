fc0706b3f1c2f1bbc5b535d842fd42f61c389fe9
Yu Wang <wangyu@in.tum.de>
2017-08-08 15:31:59 +0200

    fix indent and add new empty files

--- core/resnet38.py
+++ core/resnet38.py
@@ -2,10 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
-import os
-import numpy as np
+import nn
 import tensorflow as tf
+import numpy as np
+
+import sys
+sys.path.append("..")
 import data_utils as dt
 
+class ResNet38:
 
--- data_utils.py
+++ data_utils.py
@@ -2,10 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
 import os
 import numpy as np
-import tensorflow as tf
-import data_utils as dt
-
-
+from dataset.CIFAR10 import CIFAR10
