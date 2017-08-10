bd3c0bf13e7cd6e38066afc9b6cce89bfe608ed9
Yu Wang <wangyu@in.tum.de>
2017-08-10 15:39:45 +0200

    network and train script done, ready for debug on server

--- core/resnet38.py
+++ core/resnet38.py
@@ -11,4 +11,173 @@ sys.path.append("..")
 import data_utils as dt
 
 class ResNet38:
+    def __init__(self, weight_path=None):
+        self._weight_dict = dt.load_weight(weight_path)
+        self._var_dict = {}
+        self._num_classes = 10
+
+    def _build_model(self, image, num_classes, is_train=False):
+        '''If is_train, save weight to self._var_dict,
+           otherwise, don't save weights'''
+
+        model = {}
+        feed_dict = self._weight_dict
+        self._num_classes = num_classes
+        if is_train:
+            var_dict = self._var_dict
+        else:
+            var_dict = None
+
+        if is_train:
+            dropout = True
+        else:
+            dropout = False
+
+        shape_dict = {}
+        shape_dict['B0'] = [3,3,3,64]
+
+        # B0: [H,W,3] -> [H,W,64]
+        with tf.variable_scope('B0'):
+            model['B0'] = nn.conv_layer(image, feed_dict, 1, 'SAME',
+                                        shape_dict['B0'], var_dict)
+
+        # B2_1: [H,W,64] -> [H/2, W/2, 128]
+        shape_dict['B2'] = {}
+        shape_dict['B2']['side'] = [1,1,64,128]
+        shape_dict['B2']['convs'] = [[3,3,64,128],[3,3,128,128]]
+        with tf.variable_scope('B2_1'):
+            model['B2_1'] = nn.ResUnit_downsample_2convs(model['B0'],
+                                                         feed_dict,
+                                                         shape_dict['B2'],
+                                                         var_dict=var_dict)
+        # B2_2, B2_3: [H/2, W/2, 128]
+        for i in range(2):
+            with tf.variable_scope('B2_'+str(i+2)):
+                model['B2_'+str(i+2)] =
+                nn.ResUnit_2convs(model['B2_'+str(i+1)], feed_dict,
+                                  shape_dict['B2']['convs'][1],
+                                  var_dict=var_dict)
+
+        # B3_1: [H/2, W/2, 128] -> [H/4, W/4, 256]
+        shape_dict['B3'] = {}
+        shape_dict['B3']['side'] = [1,1,128,256]
+        shape_dict['B3']['convs'] = [[3,3,128,256],[3,3,256,256]]
+        with tf.variable_scope('B3_1'):
+            model['B3_1'] = nn.ResUnit_downsample_2convs(model['B2_3'],
+                                                         feed_dict,
+                                                         shape_dict['B3'],
+                                                         var_dict=var_dict)
+        # B3_2, B3_3: [H/4, W/4, 256]
+        for i in range(2):
+            with tf.variable_scope('B3_'+str(i+2)):
+                model['B3_'+str(i+2)] =
+                nn.ResUnit_2convs(model['B3_'+str(i+1)], feed_dict,
+                                  shape_dict['B3']['convs'][1],
+                                  var_dict=var_dict)
+        # B4_1: [H/4, W/4, 256] -> [H/8, W/8, 512]
+        shape_dict['B4'] = {}
+        shape_dict['B4']['side'] = [1,1,256,512]
+        shape_dict['B4']['convs'] = [[3,3,256,512],[3,3,512,512]]
+        with tf.variable_scope('B4_1'):
+            model['B4_1'] = nn.ResUnit_downsample_2convs(model['B3_3'],
+                                                             feed_dict,
+                                                             shape_dict['B4'],
+                                                             var_dict=var_dict)
+        # B4_2 ~ B4_6: [H/8, W/8, 512]
+        for i in range(5):
+            with tf.variable_scope('B4_'+str(i+2)):
+                model['B4_'+str(i+2)] = nn.ResUnit_2convs(model['B4_'+str(i+1)],
+                                                               feed_dict,
+                                                               shape_dict['B4']['convs'][1],
+                                                               var_dict=var_dict)
+        # B5_1: [H/8, W/8, 512] -> [H/8, W/8, 1024]
+        shape_dict['B5_1'] = {}
+        shape_dict['B5_1']['side'] = [1,1,512,1024]
+        shape_dict['B5_1']['convs'] = [[3,3,512,512],[3,3,512,1024]]
+        with tf.variable_scope('B5_1'):
+            model['B5_1'] = nn.ResUnit_hybrid_dilate_2conv(model['B4_6'],
+                                                               feed_dict,
+                                                               shape_dict['B5_1'],
+                                                               var_dict=var_dict)
+        # B5_2, B5_3: [H/8, W/8, 1024]
+        # Shape for B5_2, B5_3
+        shape_dict['B5_2_3'] = [[3,3,1024,512],[3,3,512,1024]]
+        for i in range(2):
+            with tf.variable_scope('B5_'+str(i+2)):
+                model['B5_'+str(i+2)] = nn.ResUnit_full_dilate_2convs(model['B5_'+str(i+1)],
+                                                  feed_dict, shape_dict['B5_2_3'],
+                                                  var_dict=var_dict)
+
+        # B6: [H/8, W/8, 1024] -> [H/8, W/8, 2048]
+        shape_dict['B6'] = [[1,1,1024,512],[3,3,512,1024],[1,1,1024,2048]]
+        with tf.variable_scope('B6'):
+            model['B6'] = nn.ResUnit_hybrid_dilate_3conv(model['B5_3'],
+                                                             feed_dict,
+                                                             shape_dict['B6'],
+                                                             dropout=dropout,
+                                                             var_dict=var_dict)
+        # B7: [H/8, W/8, 2048] -> [H/8, W/8, 4096]
+        shape_dict['B7'] = [[1,1,2048,1024],[3,3,1024,2048],[1,1,2048,4096]]
+        with tf.variable_scope('B7'):
+            model['B7'] = nn.ResUnit_hybrid_dilate_3conv(model['B6'],
+                                                             feed_dict,
+                                                             shape_dict['B7'],
+                                                             dropout=dropout,
+                                                             var_dict=var_dict)
+
+        # ResNet tail. No conv, only batch_norm + activation
+        shape_dict['Tail'] = 4096
+        with tf.variable_scope('Tail'):
+            model['Tail'] = nn.ResUnit_tail(model['B7'], feed_dict,
+                                            shape_dict['Tail'], var_dict)
+
+        # Global Pooling: [H/8, W/8, 4096] -> [4096]
+        with tf.variable_scope('avg_pool'):
+            model['pool_out'] = nn.Global_avg_pool(model['Tail'])
+
+        # Fully connected: [4096] -> [10]
+        with tf.variable_scope('FC'):
+            batch_size = tf.shape(model['pool_out'])[0]
+            model['fc_out'] = nn.FC(model['pool_out'], batch_size, feed_dict,
+                                    self._num_classes, var_dict)
+
+        return model
+
+    def _weight_decay(self, decay_rate):
+        '''Compute weight decay loss for convolution kernel and fully connected
+        weigths, excluding trainable variables of BN layer'''
+
+        l2_losses = []
+        for var in tf.trainable_variables():
+            if var.op.name.find('kernel') or var.op.name.find('fc_weight') or
+            var.op.name.find('fc_bias'):
+                l2_losses.append(tf.nn.l2_loss(var))
+
+        return tf.multiply(decay_rate, tf.add_n(l2_losses))
+
+    def train(self, image, label, params):
+        '''Input: Image [batch_size, H, W, C]
+                  Label [batch_size]
+                  params: 'num_class', 'batch_size', 'decay_rate' '''
+
+        model = self._build_model(image, params['num_class'], is_train=True)
+        prediction = model['fc_out']
+        label = tf.reshape(label, [params['batch_size']])
+
+        # compute train accuracy
+        pred_label = tf.argmax(tf.nn.softmax(prediction), axis=1)
+        correct_pred_bools = tf.equal(pred_label, label)
+        correct_preds = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
+        train_acc = correct_preds/params['batch_size']
+        entropy_loss =
+        tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=label,
+                                                                      logits=prediction))
+        total_loss = entropy_loss + self._weight_decay(params['decay_rate'])
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            # default learning rate for Adam: 0.001
+            train_op = tf.train.AdamOptimizer().minimize(total_loss)
+
+        return train_op, total_loss, train_acc, correct_preds
+
 
--- train.py
+++ train.py
@@ -7,5 +7,83 @@ import os
 import numpy as np
 import tensorflow as tf
 import data_utils as dt
+from core.resnet38 import ResNet38
+
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+config_gpu = tf.ConfigProto()
+config_gpu.gpu_options.per_process_gpu_memory_fraction = 1.0
+
+train_data_params = {'data_path': 'data/cifar-10-batches-py/',
+                     'batch_size': 64,
+                     'mode': 'Train'}
+dataset = dt.CIFAR10(train_data_params)
+
+params = {'num_class': 10,
+          'batch_size': 64,
+          'decay_rate': 0.0002,
+          'feed_path': '../data/trained_weights/empty.npy',
+          'save_path': '../data/saved_weights/',
+          'tsboard_save_path': '../data/tsboard/'}
+
+train_ep = 1
+val_step_iter = 5
+save_ep = 1
+
+with tf.Session(config=config_gpu) as sess:
+    res38 = ResNet38(params['feed_path'])
+    save_path = params['save_path']
+    batch_size = params['batch_size']
+
+    train_img = tf.placeholder(tf.float32, shape=[batch_size, 32, 32,
+                                                  3])
+    train_label = tf.placeholder(tf.int32, shape=[batch_size])
+
+    [train_op, total_loss, train_acc, correct_preds] = res38.train(image=train_img, label=train_label, params=params)
+
+    save_dict_op = res38._var_dict
+    TrainLoss_sum = tf.summary.scalar('train_loss', loss)
+    TrainAcc_sum = tf.summary.scalar('train_acc', train_acc)
+    Train_summary = tf.summary.merge([TrainLoss_sum, TrainAcc_sum])
+
+    train_writer = tf.summary.FileWriter(params['tsboard_save_path']+'train', sess.graph)
+    val_writer = tf.summary.FileWriter(params['tsboard_save_path']+'val', sess.graph)
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    num_iters = 45000 / batch_size + 1
+    print('Start training...')
+    for epoch in range(train_ep):
+        print('Eopch %d'%epoch)
+        for iters in range(num_iters):
+            print('iter %d'%iters)
+            if iters % val_step_iter == 0 and iters != 0:
+                # run validation
+                next_images, next_labels = train_dataset._get_TestPool()
+                val_feed_dict = {train_img: next_images, train_label:
+                                 next_labels}
+                [val_accuracy_, total_loss_] = sess.run([train_acc,total_loss], val_feed_dict)
+                val_writer.add_summary(Train_summary, iters)
+
+                # test save weight, comment later
+                print('Save trained weight after epoch: %d'%epoch)
+                save_npy = sess.run(save_dict_op)
+                if len(save_npy.keys()) != 0:
+                    save_name = 'CIFAR10_ResNet38_%d.npy'%(iters+1)
+                    save_path = save_path + save_name
+                    np.save(save_path, save_npy)
+                # test save weight ended
+            else:
+                # run training
+                next_images, next_labels = train_dataset.next_batch()
+                train_feed_dict = {train_img: next_batch_image, train_label: next_batch_label}
+                [train_op_, total_loss_, train_acc_, merged_summary_] = sess.run([train_op, total_loss, train_acc, merged_summary], train_feed_dict)
+                train_writer.add_summary(Train_summary, iters)
+                print('Iter %d loss: %f'%(iters, total_loss_))
+        # if epoch % save_ep == 0 and epoch !=0:
+        #    print('Save trained weight after epoch: %d'%epoch)
+        #    save_npy = sess.run(save_dict_op)
+        # Shuffle dataset
+        train_dataset.shuffle()
 
 
